9ab294718b560ceb926ec2144ae2a9dfbc036428
Swayam <swayamkumar86@gmail.com>
2025-05-21 02:11:00 +0530

    Resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/10Ot2kNKl9gCakXl_akLpUZZy4YMvHU_9/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
