c49b7eb6fee00003ed9b1dc42db27f5a205fbc09
Swayam <swayamkumar86@gmail.com>
2025-10-10 13:09:16 +0530

    Updating Resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/16WJCkYs5JwLXVBEeAGSIC6VT2BYaVCtb/view?usp=sharing"
+            href="https://drive.google.com/file/d/1OCI_hI80dYfxWe1DUklV4W1O9ht_Xkue/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
